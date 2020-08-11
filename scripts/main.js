5ef5d0f457d32d0b88c530b402cb940fff3a9391
LegendRK <rgk8966@g.rit.edu>
2020-08-10 23:24:49 -0700

    Cleaned up code, updated header image, added fade effects, added experience section.

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+function init() {
+    fadeInElement();
+    setupEvents();
+}
+
+function setupEvents() {
+    document.addEventListener("scroll", fadeInElement);
+}
+
+function fadeInElement() {
+    let pageTop = $(document).scrollTop();
+    let pageBottom = pageTop + $(window).height();
+    let tags = $(".fade");
+
+    for (var i = 0; i < tags.length; i++) {
+        var tag = tags[i];
+
+        if ($(tag).position().top < pageBottom) {
+            $(tag).addClass("visible");
+        } else {
+            $(tag).removeClass("visible");
+        }
+    }
+}
+
+export { init };
